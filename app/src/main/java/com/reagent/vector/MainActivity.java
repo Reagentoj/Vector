cfbbd1ee20e20d21004d72ca4ace358548504390
atatarkanov <gerronimoo@gmail.com>
2021-09-19 16:02:33 +0300

    test commit

--- app/src/main/java/com/reagent/vector/MainActivity.java
+++ app/src/main/java/com/reagent/vector/MainActivity.java
@@ -1,14 +1,25 @@
 package com.reagent.vector;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_login);
+        setContentView(R.layout.activity_main);
     }
 }
